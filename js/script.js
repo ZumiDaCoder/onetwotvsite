adbc3958f135b3c12e19913fa106e411e86c748f
ZumiDaCoder <zuhumomar@gmail.com>
2023-10-04 03:37:02 +0300

    recreated YT embed cards with Mtandao Boss

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const dialog = document.querySelector('dialog')
+const video = document.getElementById('modal-video')
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
-} )
+})
+
+
+
+
+document.querySelectorAll(".modal").forEach((button) => { 
+    button.addEventListener("click", () => { 
+        video.src = button.getAttribute("data-url")
+        dialog.showModal()
+     })
+ })
